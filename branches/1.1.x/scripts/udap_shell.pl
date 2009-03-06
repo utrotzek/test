c2d17325a0f7642ff9547036d19f2c48e3f5efcb
robin <robin@db1b1487-fd43-0410-8b57-b01607681d80>
2009-03-06 21:51:01 +0000

    Create 1.1.x branch

---
+++ branches/1.1.x/scripts/udap_shell.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/env perl
+
+# $Id$
+#
+# Copyright (c) 2008 by Robin Bowes <robin@robinbowes.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use strict;
+use warnings;
+
+# Add the modules to the libpath
+use FindBin;
+use lib "$FindBin::Bin/../src/Net-UDAP/lib";
+
+use version; our $VERSION = qv('1.0_01');
+
+use Carp;
+use Data::Dumper;
+use Net::UDAP::Shell;
+
+$| = 1;
+
+my $shell = Net::UDAP::Shell->new;
+$shell->cmdloop;
+
+# vim:set softtabstop=4:
+# vim:set shiftwidth=4:
